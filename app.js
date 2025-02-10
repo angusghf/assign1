7f116aed47fb3d6c667328d1ccd298e84108ec2c
angusghf <159850524+angusghf@users.noreply.github.com>
2025-02-09 17:06:45 -0800

    Adding Additional Comments

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+// Import express framework
 const express = require('express');
 // import the books router and save it to booksRouter
 const booksRouter = require("./routes/books")
+// create an express application
 const app = express();
+// define the server port
 const PORT = 3000;
 
+// path module for handling file paths
 const path = require('path');
 
 // 3rd party Middleware
+// parses json data
 const bodyParser = require("body-parser");
 
 // parse every req sent to server
@@ -14,13 +19,17 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 // apply all the routes in books.js to /api/ROUTES_FROM_OTHER_FULE
-    // i.e. api/books
+// i.e. api/books
 // api/books/2
+
+// uses the books router for api requests
 app.use("/api", booksRouter);
 
+// Serves static files from the public folder 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Start the server
 app.listen(PORT, () => {
+    // logging the server url
     console.log(`Server running at http://localhost:${PORT}/`);
 });
--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,9 @@
+// Import Express.framework
 const express = require("express");
 
 // create a new group of routes
+
+// create a new router instance
 const router = express.Router();
 
 // List of books with initial items
@@ -12,20 +15,24 @@ const books = [
 ];
 
 
-//Find and Return a a Book
+//Find and Return a Book
+// Middleware to find a book by ID
 function findBookById(req, res, next) {
 
     // get the id from the url
     const requestedId = Number(req.params.id);
 
     // save the matching array item to bookData if the Id of the item is the same as the requested Id
+    // Basically finds a book in the list
     const bookData = books.find((singleBook) => {
         return singleBook.id === requestedId;
     })
 
     // if it is defined, that means it exists, so attach it to the request as a new .book prop
     if (bookData !== undefined) {
+        // Attach a book to the request
         req.book = bookData;
+        // Move to the next middleware
         next();
         // send a 404 response if there is no match
     } else {
@@ -41,31 +48,34 @@ function findBookById(req, res, next) {
 // all of these used to be app.get/post/put/delete
 // they now now become router.get/post/put/delete
 router.get('/books', (req, res) => res.send(books));
+// Get a single book by Id
 router.get('/books/:id', findBookById, (req, res) => res.send(req.book));
 
 // Add a new book via POST
 router.post('/books', (req, res) => {
     const book = req.body
+    // Assigns a new Id
     book.id = books.length + 1;
+    // Adds to the list
     books.push(book);
-
+    // sends confirmation
     res.status(201).send(book);
 });
 
 // Delete a book by id via a DELETE request
 router.delete('/books/:id', findBookById, (req, res) => {
-
-    // removed the todo object from the group
+    // removed the book from list
     books.splice((req.book.id - 1), 1);
 
-    // Respond with a success message
+    // Respond with a success message if it works properly
     res.status(204).send('Book deleted');
 
 });
 
+// Update a book by Id
 router.put('/books/:id', findBookById, (req, res) => {
-
     const updatedData = req.body;
+    // Update book details
     req.book.text = updatedData.text;
 
     // Respond with the updated book and a status of 200
